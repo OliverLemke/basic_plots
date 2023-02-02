076ecd6a3b33da1aca4495d49a2b23f54067b887
OliverLemke <30410271+OliverLemke@users.noreply.github.com>
2023-02-02 17:05:13 +0100

    Added Correlation Boxplot

--- Basic_plots.py
+++ Basic_plots.py
@@ -15,9 +15,10 @@ from matplotlib.offsetbox import AnchoredText
 import matplotlib as mpl
 from statsmodels.stats.multitest import multipletests
 from sklearn.metrics import roc_curve, roc_auc_score
+import re
 
-
-def plot_hist(data_frame, keys, outfile="Out_hist.pdf", x_label="x", y_label="y", fs=15, n_bins=20, smoothing_factor=0.01, legend_loc="upper left", x_lim=None, grid=True):
+# Histogram
+def plot_hist(data_frame, keys, outfile="Out_hist.pdf", x_label="x", y_label="y", fs=15, fs_legend=15, n_bins=20, smoothing_factor=1e-10, legend_loc="upper left", x_lim=None, grid=True):
     
     fig, ax = plt.subplots()
     fig.set_size_inches(6,4)
@@ -54,10 +55,10 @@ def plot_hist(data_frame, keys, outfile="Out_hist.pdf", x_label="x", y_label="y"
     ax.tick_params(axis="both", labelsize=fs)
     
     try:
-        legend = plt.legend(loc=legend_loc, fontsize=fs, shadow=True, fancybox=True, framealpha=1)
+        legend = plt.legend(loc=legend_loc, fontsize=fs_legend, shadow=True, fancybox=True, framealpha=1)
     except:
         print("legend_loc not found. Using upper left as a default.")
-        legend = plt.legend(loc="upper left", fontsize=fs, shadow=True, fancybox=True, framealpha=1)
+        legend = plt.legend(loc="upper left", fontsize=fs_legend, shadow=True, fancybox=True, framealpha=1)
     legend.get_frame().set_linewidth(2)
 
     for legobj in legend.legendHandles:
@@ -66,7 +67,70 @@ def plot_hist(data_frame, keys, outfile="Out_hist.pdf", x_label="x", y_label="y"
     plt.tight_layout()
     plt.savefig(outfile)
     
+def plot_hist_selection(data_frame, selections, ref_key, outfile="Out_hist_selection.pdf", x_label="x", y_label="y", fs=15, fs_legend=15, n_bins=20, smoothing_factor=1e-10, legend_loc="upper left", x_lim=None, grid=True, plot_full=True):
     
+    fig, ax = plt.subplots()
+    fig.set_size_inches(6,4)
+
+    max_y = 0
+    key_x = list(ref_key.keys())[0]
+    
+    if not x_lim:
+        x_lim = (np.nanmin(data_frame[key_x].values)-(0.05*np.nanmax(data_frame[key_x].values)),np.nanmax(data_frame[key_x].values)+(0.05*np.nanmax(data_frame[key_x].values)))
+    
+    for selection in selections:
+
+        hist = np.histogram(data_frame.loc[selections[selection]["Indices"],key_x].values, range=x_lim, bins=n_bins, density=True)
+        x = (hist[1][1:]+hist[1][:-1])/2
+
+        spl = UnivariateSpline(np.insert(x,len(x),x_lim[1]),np.insert(hist[0],len(hist[0]),hist[0][-1]))
+        spl.set_smoothing_factor(smoothing_factor)
+        
+        xs = np.linspace(x_lim[0],x_lim[1],1000)
+        
+        ax.plot(xs, spl(xs), color=selections[selection]["Color"], label=selections[selection]["Label"])
+        ax.fill_between(xs, np.zeros(len(xs)), spl(xs), color=selections[selection]["Color"], alpha=.6)
+        
+        max_y = np.nanmax((max_y,np.nanmax(hist[0])))
+    
+    if plot_full:
+        hist = np.histogram(data_frame[key_x].values, range=x_lim, bins=n_bins, density=True)
+        x = (hist[1][1:]+hist[1][:-1])/2
+
+        spl = UnivariateSpline(np.insert(x,len(x),x_lim[1]),np.insert(hist[0],len(hist[0]),hist[0][-1]))
+        spl.set_smoothing_factor(smoothing_factor)
+        
+        xs = np.linspace(x_lim[0],x_lim[1],1000)
+        
+        ax.plot(xs, spl(xs), color=ref_key[key_x]["Color"], label=ref_key[key_x]["Label"])
+        ax.fill_between(xs, np.zeros(len(xs)), spl(xs), color=ref_key[key_x]["Color"], alpha=.6)
+        
+        max_y = np.nanmax((max_y,np.nanmax(hist[0])))
+    
+    if grid:
+        ax.grid(axis='both', color='0.8')
+           
+    ax.set_xlim(x_lim)           
+    ax.set_ylim(0,max_y*1.1)
+
+    ax.set_xlabel(x_label, fontsize=fs)
+    ax.set_ylabel(y_label, fontsize=fs)
+    ax.tick_params(axis="both", labelsize=fs)
+    
+    try:
+        legend = plt.legend(loc=legend_loc, fontsize=fs_legend, shadow=True, fancybox=True, framealpha=1)
+    except:
+        print("legend_loc not found. Using upper left as a default.")
+        legend = plt.legend(loc="upper left", fontsize=fs_legend, shadow=True, fancybox=True, framealpha=1)
+    legend.get_frame().set_linewidth(2)
+
+    for legobj in legend.legendHandles:
+        legobj.set_linewidth(5)
+
+    plt.tight_layout()
+    plt.savefig(outfile)
+    
+# Scatter    
 def plot_correlation_scatter(data_frame, keys, outfile="Out_correlation_scatter.pdf", fs=20, fs_text=15, n_bins=20, smoothing_factor=1e-10, text_loc="lower right", color = "C0", pearson = True, spearman = True, p_pearson = None, p_spearman = None, x_lim = None, y_lim = None, plot_linreg = True, plot_xy = False, grid = True):
 
     # Add second layer (alpha=1, color_second_layer="C1", hist second layer normalized to max of everything)    
@@ -180,60 +244,6 @@ def plot_correlation_scatter(data_frame, keys, outfile="Out_correlation_scatter.
     
     ax_hist_y.set_ylim(y_lim)     
     ax_hist_y.set_xlim(0,np.nanmax(hist[0])*1.1)
-    
-    plt.savefig(outfile, bbox_inches="tight")
-
-def plot_binned_average(data_list, N=100, outfile="Out_binned_average", cmap = "viridis", fs = 15, x_label = "rel. Index", y_label="Mean(y)", boundaries=None, y_lim=None, grid = True):
-
-    data_grouped = np.asarray([[np.nanmean(item[int(np.ceil((n)*len(item)/N)):int(np.ceil((n+1)*len(item)/N))]) for n in range(N)] for item in data_list])
-    mean_grouped = np.nanmean(data_grouped, axis=0)
-    std_grouped = np.nanstd(data_grouped, axis=0)
-    cv_grouped = std_grouped/mean_grouped
-
-    try:
-        cm = mpl.cm.get_cmap(cmap)
-    except:
-        raise ValueError("Color map not found")
-        
-    min_cv = np.min(cv_grouped)
-    max_cv = np.max(cv_grouped)
-    
-    fig = plt.figure()
-    fig.set_size_inches(6,4)
-    gs = gridspec.GridSpec(1,2, width_ratios=[5,1])
-    
-    xs = np.arange((1/(2*N)),1+(1/(2*N)),1/N)
-    
-    ax = fig.add_subplot(gs[0,0])
-    for ind in range(N-1):
-        ax.plot([xs[ind],xs[ind+1]], [mean_grouped[ind],mean_grouped[ind+1]], lw=3, c=cm((((cv_grouped[ind]+cv_grouped[ind+1])/2)-min_cv)/(max_cv-min_cv)))
-        ax.fill_between([xs[ind],xs[ind+1]], [mean_grouped[ind]-std_grouped[ind],mean_grouped[ind+1]-std_grouped[ind+1]],[mean_grouped[ind]+std_grouped[ind],mean_grouped[ind+1]+std_grouped[ind+1]], color=cm((((cv_grouped[ind]+cv_grouped[ind+1])/2)-min_cv)/(max_cv-min_cv)), alpha=0.6)
-    
-    if boundaries:
-        for boundary in boundaries:
-            ax.plot([0,1],[boundary,boundary],c="k",ls="--")
-            
-    ax.set_xlabel(x_label, fontsize=fs)
-    ax.set_ylabel(y_label, fontsize=fs)
-    
-    ax.set_xlim(0,1)
-    if y_lim:
-        ax.set_ylim(y_lim)
-    
-    ax.tick_params(axis="both",labelsize=fs)
-    
-    if grid:
-        ax.grid(axis='both', color='0.8')
-        
-    cax = fig.add_subplot(gs[0,1])
-    cax.matshow(np.linspace(min_cv,max_cv,101).reshape(-1,1), cmap=cm, aspect=0.1)
-    
-    cax.set_xticks([])
-    
-    cax.yaxis.tick_right()
-    cax.set_yticks(np.linspace(0,100,6), [str(round(float(label), 2)) for label in (np.linspace(0,1,6)*(max_cv-min_cv))+min_cv], fontsize=fs)
-    
-    cax.set_ylabel("Coefficient of variation", fontsize=fs, rotation=90)
     
     plt.savefig(outfile, bbox_inches="tight")
     
@@ -361,7 +371,99 @@ def plot_correlations_grid(data_frame, keys, ref_key, outfile = "Out_correlation
     ax_y.text(0.0,0.5,y_label, fontsize=fs, rotation=90, horizontalalignment='left', verticalalignment='center')
     
     plt.savefig(outfile, bbox_inches="tight")
+
+def plot_correlations_boxplot(data_frame, keys, ref_key, corr_type="Spearman", outfile="Out_boxplot.pdf", fs=15, y_lim=None, grid=True):
+
+    key_x = list(ref_key.keys())[0]
+    
+    if corr_type=="Spearman":
+        collection_R = [[spearmanr(data_frame.copy()[[key_x,column]].dropna()[key_x],data_frame.copy()[[key_x,column]].dropna()[column])[0] for column in [column for column in data_frame.columns if re.findall(key,column)]] for key in keys]
+    elif corr_type=="Pearson":
+        collection_R = [[pearsonr(data_frame.copy()[[key_x,column]].dropna()[key_x],data_frame.copy()[[key_x,column]].dropna()[column])[0] for column in [column for column in data_frame.columns if re.findall(key,column)]] for key in keys]
+    else:
+        raise ValueError("Correlation type not defined.")
+    
+    
+    fig,ax = plt.subplots()
+    fig.set_size_inches(len(keys)/3+1,5)
+    bplot = ax.boxplot(collection_R,patch_artist=True,medianprops=dict(color="k"))
+    
+    colors = [keys[key]["Color"] for key in keys]
+    for patch, color in zip(bplot['boxes'], colors):
+            patch.set_facecolor(color)
+    
+    ax.plot([0.5,20.5],[0,0],c="k",ls=":")
+    
+    if grid:
+        ax.grid(axis='both', color='0.8')
+    
+    if not y_lim:
+        y_lim=((-1)*np.nanmax(np.abs(collection_R))-0.05*np.nanmax(np.abs(collection_R)),np.nanmax(np.abs(collection_R))+0.05*np.nanmax(np.abs(collection_R)))
+    
+    ax.set_xticklabels(list(keys.keys()),rotation=90, fontsize=fs)
+    ax.set_ylim(y_lim)
+    ax.set_ylabel(corr_type+"R "+ref_key[key_x]["Label"], fontsize=fs)
+    
+    ax.tick_params(axis="y",labelsize=fs)
+    
+    plt.savefig(outfile, bbox_inches="tight")
+
+# Average
+def plot_binned_average(data_list, N=100, outfile="Out_binned_average", cmap = "viridis", fs = 15, x_label = "rel. Index", y_label="Mean(y)", boundaries=None, y_lim=None, grid = True):
+
+    data_grouped = np.asarray([[np.nanmean(item[int(np.ceil((n)*len(item)/N)):int(np.ceil((n+1)*len(item)/N))]) for n in range(N)] for item in data_list])
+    mean_grouped = np.nanmean(data_grouped, axis=0)
+    std_grouped = np.nanstd(data_grouped, axis=0)
+    cv_grouped = std_grouped/mean_grouped
+
+    try:
+        cm = mpl.cm.get_cmap(cmap)
+    except:
+        raise ValueError("Color map not found")
         
+    min_cv = np.min(cv_grouped)
+    max_cv = np.max(cv_grouped)
+    
+    fig = plt.figure()
+    fig.set_size_inches(6,4)
+    gs = gridspec.GridSpec(1,2, width_ratios=[5,1])
+    
+    xs = np.arange((1/(2*N)),1+(1/(2*N)),1/N)
+    
+    ax = fig.add_subplot(gs[0,0])
+    for ind in range(N-1):
+        ax.plot([xs[ind],xs[ind+1]], [mean_grouped[ind],mean_grouped[ind+1]], lw=3, c=cm((((cv_grouped[ind]+cv_grouped[ind+1])/2)-min_cv)/(max_cv-min_cv)))
+        ax.fill_between([xs[ind],xs[ind+1]], [mean_grouped[ind]-std_grouped[ind],mean_grouped[ind+1]-std_grouped[ind+1]],[mean_grouped[ind]+std_grouped[ind],mean_grouped[ind+1]+std_grouped[ind+1]], color=cm((((cv_grouped[ind]+cv_grouped[ind+1])/2)-min_cv)/(max_cv-min_cv)), alpha=0.6)
+    
+    if boundaries:
+        for boundary in boundaries:
+            ax.plot([0,1],[boundary,boundary],c="k",ls="--")
+            
+    ax.set_xlabel(x_label, fontsize=fs)
+    ax.set_ylabel(y_label, fontsize=fs)
+    
+    ax.set_xlim(0,1)
+    if y_lim:
+        ax.set_ylim(y_lim)
+    
+    ax.tick_params(axis="both",labelsize=fs)
+    
+    if grid:
+        ax.grid(axis='both', color='0.8')
+        
+    cax = fig.add_subplot(gs[0,1])
+    cax.matshow(np.linspace(min_cv,max_cv,101).reshape(-1,1), cmap=cm, aspect=0.1)
+    
+    cax.set_xticks([])
+    
+    cax.yaxis.tick_right()
+    cax.set_yticks(np.linspace(0,100,6), [str(round(float(label), 2)) for label in (np.linspace(0,1,6)*(max_cv-min_cv))+min_cv], fontsize=fs)
+    
+    cax.set_ylabel("Coefficient of variation", fontsize=fs, rotation=90)
+    
+    plt.savefig(outfile, bbox_inches="tight")
+        
+# Enrichment    
 def plot_enrichment(data_frame, keys, p_column, size_column, outfile = "../Out_enrichment.pdf", fs = 15, auc_column=None, cmap="Reds", splits=None, already_log10_transformed=False, v_lim=None, cbar_resolution = 4, num_legend_labels = 3):
 
     if not already_log10_transformed:
@@ -458,8 +560,6 @@ def plot_AUC(data_frame_keys, keys, data_frame_ref_keys, ref_keys, outfile="Out_
             plt.savefig(outfile[:-4]+"_"+str(ind)+outfile[-4:],bbox_inches="tight")  
         else:
             plt.savefig(outfile,bbox_inches="tight")  
-        
-        
         
         
         
